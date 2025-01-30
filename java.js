8dc8b8dafc5e6d34f674fe4a6ef6ed2edd5bdc70
olan0003 <olan0003@stud.kea.dk>
2025-01-30 10:10:49 +0100

    ændringer

--- java.js
+++
@@ -1,5 +0,0 @@
-const minKnap = document.querySelector("#knapElement");
-minKnap.addEventListener("click", funktionDerKaldesVedKlik);
-function funktionDerKaldesVedKlik() {
-  console.log("du har klikket på knappen");
-}
